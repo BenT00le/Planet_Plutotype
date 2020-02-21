ac1d5901cde4d2fe02418c6cd6516e7be9b20c6b
BenT00le <ben4toole@gmail.com>
2020-02-21 14:57:32 -0800

    example projects for user auth and node

---
+++ test_express/client/src/dragable.js
@@ -0,0 +1,13 @@
+function Dragable({name, isHit, color})
+{
+	const [hitCount, setHit] = useState(0);
+	const debug = () =>
+	{
+		console.log('debug '+hitCount);
+		setHit(hitCount+1);
+	};
+
+	return( 
+
+	);
+}
