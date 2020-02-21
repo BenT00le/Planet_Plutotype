4cd921a2ed7ed2c479da52c813dec177c63b8ab4
BenT00le <ben4toole@gmail.com>
2020-02-21 15:17:12 -0800

    added react files we should be back up to date now

---
+++ src/Day.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Day()
+{
+	return(
+
+
+	);
+}
+
+export default Day
---
+++ src/SubtaskPopup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function SubtaskPopup()
+{
+	return(
+
+
+	);
+}
+
+export default SubtaskPopup
