3c5b0ec640ffcaca89fe0901221cef597a68837e
Bennett Lopez <bennettlopez01@gmail.com>
2020-03-03 10:35:47 -0800

    This is an update the the HTML/JS/CSS for the index page

---
+++ user_auth/Revised HTML/index.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+    
+    
+    
+    
+    
+    
+    
+    </head>
+    <body>
+        <p>This is the starting document</p>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    </body>
+</html>
