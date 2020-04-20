61063f7e5ca24381dd29d7b2e1d45a5256a508b9
BenT00le <ben4toole@gmail.com>
2020-04-20 00:46:53 -0700

    ignore node_modules

--- plutotype_nodejs/nodeTest/http.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-//server object
-http.createServer((req, res) => {
-
-    res.write('text in browser');
-    res.write('enter password');
-    res.end()
-}).listen(50069, () => console.log('server running'));
